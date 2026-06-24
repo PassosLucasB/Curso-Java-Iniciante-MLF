f1589246a9876b0f41809027268545fa5909fbd7
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2026-06-23 23:12:51 -0300

    Add files via upload

---
+++ src/Teste.java
@@ -0,0 +1,26 @@
+public class Teste {
+
+    public static void main(String[] args) {
+
+        int idade = 38;
+        Integer idadeObjeto = 38;
+        double altura = 1.94;
+        Double alturaObjeto = 1.94;
+        float alturaF = 1.94F;
+        Float alturaObjeto = 1.94F;
+        String nome = "Maria";
+        char genero = 'M';
+        Character generoObjeto = 'M';
+        boolean verdadeiro = true;
+        Boolean verdadeiroObjeto = true;
+
+
+        System.out.println("Altura: " + altura);
+        System.out.println("Altura: " + alturaF);
+        System.out.println("Nome: " + nome);
+        System.out.println("Genero: " + genero);
+        System.out.println("Verdadeiro: " + verdadeiro);
+
+
+    }
+}
