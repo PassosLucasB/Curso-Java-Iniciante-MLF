3dc3f8a8a2cc58e5f701f5bcd890293922cfb116
PassosLucasB <passosl@outlook.com>
2026-06-24 23:04:29 -0300

    Finaliza Tipos de Dados

--- src/Teste.java
+++ src/Teste.java
@@ -2,17 +2,31 @@ public class Teste {
 
     public static void main(String[] args) {
 
+        String nome = "MLF";
+        int idade = 38;
+        double altura = 1.94;
+
+        System.out.println("Nome: " + nome + "\nIdade: " + idade + "\nAltura: " + altura);
+
+
+
+
+        /*
         int idade = 38;
         Integer idadeObjeto = 38;
         double altura = 1.94;
         Double alturaObjeto = 1.94;
         float alturaF = 1.94F;
-        Float alturaObjeto = 1.94F;
+        Float alturaObjetoF = 1.94F;
         String nome = "Maria";
         char genero = 'M';
         Character generoObjeto = 'M';
         boolean verdadeiro = true;
         Boolean verdadeiroObjeto = true;
+
+        String numero = "3";
+        int numeroInt = Integer.parseInt(numero);
+        System.out.println(numeroInt);
 
 
         System.out.println("Altura: " + altura);
@@ -20,6 +34,8 @@ public class Teste {
         System.out.println("Nome: " + nome);
         System.out.println("Genero: " + genero);
         System.out.println("Verdadeiro: " + verdadeiro);
+        */
+
 
 
     }
