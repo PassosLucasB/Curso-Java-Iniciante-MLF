0f35a3d6bdd2f116c9b1b9c2ec1e27711beefe75
PassosLucasB <passosl@outlook.com>
2026-06-25 23:11:14 -0300

    Scanner

---
+++ src/Media.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+public class Media {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite a nota 1: ");
+        float nota1 = sc.nextFloat();
+
+        System.out.println("Digite a nota 2: ");
+        float nota2 = sc.nextFloat();
+
+        System.out.println("Digite a nota 3: ");
+        float nota3 = sc.nextFloat();
+
+        float media = (nota1 + nota2 + nota3) /3;
+
+        System.out.println("A média é: " + media);
+
+
+
+        /*
+        double nota1 = 10;
+        double nota2 = 8;
+        double nota3 = 7;
+        double media = (nota1 + nota2 + nota3) / 3;
+
+        System.out.println("A média é: " + media);
+        */
+    }
+}
