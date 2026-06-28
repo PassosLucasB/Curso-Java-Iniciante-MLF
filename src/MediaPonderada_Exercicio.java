34e9eebb4aaaeefd1f77046f3c668e7784924320
PassosLucasB <passosl@outlook.com>
2026-06-27 23:49:57 -0300

    Add Media Ponderada Exercicio

---
+++ src/MediaPonderada_Exercicio.java
@@ -0,0 +1,20 @@
+
+/*
+
+1- Faça um algoritmo em Java que receba três notas e seus respectivos pesos,
+calcule e mostra a média ponderada dessas notas.
+
+*/
+public class MediaPonderada_Exercicio {
+    public static void main(String[] args) {
+
+    // Mp = x1*k1 + x2*k2 + ...) / (k1 + k2 + ....)
+
+
+
+
+
+
+
+    }
+}
