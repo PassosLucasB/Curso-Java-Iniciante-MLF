6a532e3d82e13721549ce2fccf13d2868c8b27aa
PassosLucasB <passosl@outlook.com>
2026-07-02 00:03:39 -0300

    Add Condicionais If, Else If e Else

--- src/Condicionais.java
+++ src/Condicionais.java
@@ -1,16 +1,22 @@
+import java.sql.SQLOutput;
+
 public class Condicionais {
     public static void main(String[] args) {
 
-        double nota1  = 10;
-        double nota2 = 8;
-        double nota3 = 5;
+        double nota1  = 8;
+        double nota2 = 2;
+        double nota3 = 3;
         double media = (nota1 + nota2 + nota3) / 3;
 
         System.out.println("A média é : " + media);
 
         if (media >= 6){
             System.out.println("Aluno aprovado");
-        } else {
+        }
+        else if (media >= 5 && media < 6){
+            System.out.println("Aluno recuperação");
+        }
+        else{
             System.out.println("Aluno reprovado");
         }
     }
