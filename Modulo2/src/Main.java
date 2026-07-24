3808baf562b38bd809c834743723fcf3eadc6af9
PassosLucasB <passosl@outlook.com>
2026-07-23 23:45:20 -0300

    Add Modulo2 POO

---
+++ Modulo2/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Pessoa p = new Pessoa();
+        p.nome = "Matheus";
+        p.idade = 20;
+        p.altura = 1.70;
+        p.cpf = "123.456.789-00";
+    }
+}
---
+++ Modulo2/src/Pessoa.java
@@ -0,0 +1,12 @@
+public class Pessoa {
+
+    public String nome;
+    public int idade;
+    public double altura;
+    public String cpf;
+
+    /*  public = livre acesso;
+        private = apenas a mesma classe tem acesso;
+        protected = apenas classes do mesmo pacote ou herança têm acesso
+     */
+}
